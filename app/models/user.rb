30cd8ca876e34f5075d55f672bba281f2ec97012
positivelyjon <positivelyjon@gmail.com>
2023-12-30 16:49:46 +0800

    Fix bug where User uniqueness not working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     validates :username, presence: true
-    validates :username, unique: true
+    validates :username, uniqueness: true
     validates :username, length: {minimum:1}
 end
