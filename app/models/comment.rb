2e5c216ecf91493b25a44c3a8c1004228195eab2
positivelyjon <positivelyjon@gmail.com>
2023-12-30 15:58:01 +0800

    Create comment model validations and migration

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,3 @@
 class Comment < ApplicationRecord
+    validates :content, :post_id, :user_id, presence: true
 end
--- db/migrate/20231230075144_create_comments.rb
+++ db/migrate/20231230075144_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
-
+      t.text :content
+      t.references :post
+      t.references :user
       t.timestamps
     end
   end
