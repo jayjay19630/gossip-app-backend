94bea2ec4bb641d7e6e1367404fb616e2c4de0fe
positivelyjon <positivelyjon@gmail.com>
2023-12-30 17:40:51 +0800

    Create Users controller to save username data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    
+    #user controller saves username input into database, unless username is invalid
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: user, status: 201
+        else
+            render json: { error: user.errors.messages }, status: 422
+        end
+    end
+    
+    private
+    def user_params
+        params.require(:user).permit(:username)
+    end
 end
