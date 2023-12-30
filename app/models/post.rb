e8254d08326adaf926824d1bbc328beca4fcd6df
positivelyjon <positivelyjon@gmail.com>
2023-12-30 22:44:35 +0800

    Create associations between users, comments and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
     validates :title, presence: true 
     has_many :post_tags, dependent: :destroy
     has_many :tags, through: :post_tags
+    has_many :comments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     validates :username, presence: true
     validates :username, uniqueness: true
     validates :username, length: {minimum:1}
+    has_many :posts, dependent: :destroy
+    has_many :comments, dependent: :destory
 end
