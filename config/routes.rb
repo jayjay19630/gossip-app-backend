9d9bf6ffb5cb332682ccd4ee511056723eaa904e
positivelyjon <positivelyjon@gmail.com>
2023-12-30 18:07:09 +0800

    Add seeds for post tags

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   
   #route for when user submits signup form
   post 'user', to: "users#create"
+  post 'post', to: "posts#create"
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+["Studying", "Gaming", "Teaching", "Relationships", "Careers", "Computer Science"].each do |tag_name|
+    Tag.create(tag_name: tag_name)
+end
