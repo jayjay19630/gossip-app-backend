b609501c8c0680b6ee5fa3f9321b94683d76401d
positivelyjon <positivelyjon@gmail.com>
2024-01-11 18:12:51 +0800

    Change seed to have less samples and include sample likes

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_11_094958) do
     t.string "title"
     t.text "content"
     t.bigint "user_id", null: false
-    t.integer "likes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,16 +15,6 @@ titles = [
     "can anybody tell me how o(n) order of growth works?",
     "oops",
     "yikes, my grades",
-    "sample",
-    "sample",
-    "sample",
-    "sample",
-    "sample",
-    "sample",
-    "sample",
-    "sample",
-    "sample",
-    "sample",
 ]
 posts_content = [
     "i love spiderman so much he is my idol i love spiderman so much he is my idol i love spiderman so much he is my idol i love spiderman so much he is my idol i love spiderman so much he is my idol",
@@ -32,24 +22,21 @@ posts_content = [
     "i've been studying this in my computer science classes (CS1101S) recently and I have no clue how it works. i tried asking the professor, but im still confused with the subject, what do i do?",
     "i posted this on accident",
     "how to cope with all this stress? i failed all my modules this sem and i need to graduate next sem, I need to get a lot of work done too. is there any method you guys use to cope during these kinds of situations?",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 ]
-likes = [0, 0, 3, 5, 100, 0, 0, 0, 3, 6, 10, 13, 13, 14, 15]
-user_ids = [1, 2, 1, 4, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]
-tag_ids = [[4, 8], [4, 8], [1, 7], [], [1, 7, 9, 10], [], [], [], [1, 5], [6, 7], [], [4, 5], [], [2, 3], []]
+user_ids = [1, 2, 1, 4, 5]
+tag_ids = [[4, 8], [4, 8], [1, 7], [], [1, 7, 9, 10]]
+
+likes = [[1, 3], [1, 4], [2, 6], [3, 7], [4, 1], [1, 6]]
 
 titles.length().times.each do |i|
-    Post.create(title: titles[i], content: posts_content[i], user_id: user_ids[i], likes: likes[i])
+    Post.create(title: titles[i], content: posts_content[i], user_id: user_ids[i])
     tag_ids[i].each do |tag_id|
         PostTag.create(post_id: i+1, tag_id: tag_id)
     end
+end
+
+likes.length().times.each do |i|
+    liked_post_id = likes[i][0]
+    liking_user_id = likes[i][1]
+    Like.create(user_id: liking_user_id, post_id: liked_post_id)
 end
