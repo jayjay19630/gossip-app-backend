73afcea10bc23b547ab21a0886abf8fafc6e2431
positivelyjon <positivelyjon@gmail.com>
2024-01-22 19:17:47 +0800

    Allow cross origin between backend and netlify site

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,4 +3,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       origins 'http://localhost:5173'
       resource '*', headers: :any, methods: [:get, :post, :delete, :put]
     end
+    allow do
+      origins 'https://nusgrapevine.netlify.app'
+      resource '*', headers: :any, methods: [:get, :post, :delete, :put]
+    end
   end
